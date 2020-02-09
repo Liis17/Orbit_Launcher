56bc859b313b29a9c70f765d04e1cdfc239c9328
Alex D <daskent96@gmail.com>
2020-02-09 16:51:09 +0300

    ничего не работает пока

---
+++ Orbit Launcher/LinkUpdate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace Orbit_Launcher
+{   
+    class LinkUpdate
+    {
+        public string[] link;
+        public static void Updatelink()
+        {
+            MainLinkDownload();
+        }
+
+        public static void MainLinkDownload()
+        {
+            Task.Run(() =>
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("https://github.com/Liis17/Orbit_Launcher/releases/download/1/link.all.txt", "MainLink.txt");
+                    AllLinkSearch();
+                }
+            });
+        } // загрузка списка ссылок на ссылки файлов
+
+        public static void AllLinkSearch()
+        {
+            var path = "MainLink.txt"; //название главного файла со ссылками
+            var AllValue = File.ReadAllText(path); // чтение этого файла
+            var array = AllValue.Split('^'); // разделение на строки (1 строка одна ссылка)
+            var array2 = array[0].Split('|'); // разделение строки на части (рабочая только вторая)
+            AllLinkDownload();
+        } // поиск ссылки в главном файле со ссылками
+
+        public static void AllLinkDownload()
+        {
+            Task.Run(() =>
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("https://github.com/Liis17/Orbit_Launcher/releases/download/1/link.all.txt", "MainLink.txt");
+                    AllLinkDownload();
+                }
+            });
+        }
+
+
+
+
+        
+
+    }
+}
--- Orbit Launcher/MainWindow.xaml.cs
+++ Orbit Launcher/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,43 @@ namespace Orbit_Launcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string[] graylink; // ссылка на Gray
+
+        
         public MainWindow()
         {
-            InitializeComponent(); // типа инициализация компонентов
+            FirstWindow firstWindow = new FirstWindow();
+            firstWindow.Show();
+            InitializeComponent();
+            LinkUpdate.MainLinkDownload();
+        }
+
+        public async void Downloadmaster(object sender, RoutedEventArgs e)
+        {
+            //var count = graylink.Length;
+
+            using (var client = new WebClient())
+           {
+                client.DownloadProgressChanged += Client_DownloadProgressChanged;
+                client.DownloadDataCompleted += Client_DownloadDataCompleted;
+                await client.DownloadFileTaskAsync("https://github.com/Liis17/StartellingerBee/releases/download/1.0.1.9/app.publish.rar", "fi1le.rar");
+           }
+        }
+
+        private void Client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            //todo: ready.
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            var currentBytes = (int)e.TotalBytesToReceive;
+            var allBytes = (int)e.BytesReceived;
+            ValueDownload.Text = allBytes / 1024 + "/" + currentBytes / 1024;
+            ProgressBarTest.Maximum = currentBytes / 100;
+            ProgressBarTest.Value = allBytes / 100;
+
+           
         }
     }
 }
