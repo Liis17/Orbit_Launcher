b3b945adc4e3b678141c631afdc44726e0c0b21a
Alex D <daskent96@gmail.com>
2020-03-06 23:01:36 +0300

    +1

--- OrbitLauncher/MainWindow.xaml.cs
+++ OrbitLauncher/MainWindow.xaml.cs
@@ -4,6 +4,9 @@ using System.Diagnostics;
 using System.Windows;
 using System;
 
+
+using System.Windows.Forms;
+
 namespace Orbit_Launcher
 {
     /// <summary>
@@ -18,15 +21,36 @@ namespace Orbit_Launcher
         {
 
             InitializeComponent();
+            Orpad_check();
+            check_install();
 
         }
 
+        #region Загрузка блокнота
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             DownloadOrpad();
         } //нажатие на кнопку загрузки в окне блокнота
+        private void OrpadUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateOrpad();
+        } //нажание на кнопку обновить
+        public void Orpad_check()
+        {
+            var file = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\" + "Orbit_in_Space" + "\\" + "Orpad" + "\\" + "Orpad.exe";
+            if (File.Exists(file) == true)
+            {
+                OrpadDownloadButton.IsEnabled = false;
+            }
+            if (File.Exists(file) == false)
+            {
+                OrpadDownloadButton.IsEnabled = true;
+            }
+
+        } //проверка на наличение установленого блокнота
         public async void DownloadOrpad()
         {
+            OrpadDownloadButton.IsEnabled = false;
             using (var client = new WebClient())
             {
                 client.DownloadProgressChanged += Client_DownloadProgressChanged;
@@ -35,8 +59,42 @@ namespace Orbit_Launcher
                 var Link = "https://github.com/Liis17/Orpad/releases/download/fix_beta/Orpad.exe";
 
                 await client.DownloadFileTaskAsync(Link, Folder);
+                Orpad_check();
             }
+            OrpadDownloadButton.IsEnabled = true;
+            Orpad_check();
+            check_install();
         } // загрузка блокнота
+        public void check_install()
+        {
+            var file = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\" + "Orbit_in_Space" + "\\" + "Orpad" + "\\" + "Orpad.exe";
+            if (File.Exists(file) == true)
+            {
+                OrpadUpdateButton.IsEnabled = true;
+            }
+            if (File.Exists(file) == false)
+            {
+                OrpadUpdateButton.IsEnabled = false;
+            }
+        } // управление кнопкой обновления блокнота
+        public async void UpdateOrpad()
+        {
+            OrpadUpdateButton.IsEnabled = false;
+            using (var client = new WebClient())
+            {
+                client.DownloadProgressChanged += Client_DownloadProgressChanged;
+
+                var Folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\" + "Orbit_in_Space" + "\\" + "Orpad" + "\\" + "Orpad.exe";
+                var Link = "https://github.com/Liis17/Orpad/releases/download/fix_beta/Orpad.exe";
+
+                await client.DownloadFileTaskAsync(Link, Folder);
+                Orpad_check();
+            }
+            OrpadUpdateButton.IsEnabled = true;
+            Orpad_check();
+        } // обновление блокнота
+
+
 
         public void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
@@ -49,11 +107,24 @@ namespace Orbit_Launcher
             PBOrpad.Maximum = maxdown;
         } // для прогресс бара загрузки блокнота
 
+        #endregion
+
+        #region дебаг
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             PBOrpad.Value = testslider.Value;
         } // для дебага, удалить
+        #endregion
 
-        
+        //if (System.Windows.Forms.MessageBox.Show("Подтвердите переход по ссылке 'https://ww3.orbitinspace.site/program/Orpad'",
+        //    "Узнать больше",
+        //    MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+        //{
+        //    //
+        //}
+        //else
+        //{
+        //    //
+        //}
     }
 }
