779daee47351b3b72abca2b5970f382f99f73283
Li_is <daskent96@gmail.com>
2020-08-29 09:13:11 +0300

    какие то изменения

--- Orbit_Launcher_3.0/MainWindow.xaml.cs
+++ Orbit_Launcher_3.0/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using sf = Orbit_Launcher_3._0.StringField;
 
 namespace Orbit_Launcher_3._0
 {
@@ -22,7 +24,16 @@ namespace Orbit_Launcher_3._0
     {
         public MainWindow()
         {
+            CreateFolder();
             InitializeComponent();
+        }
+
+        public void CreateFolder()
+        {
+            Directory.CreateDirectory(sf.mainfolder);
+            Directory.CreateDirectory(sf.tempfolder);
+            MessageBox.Show(sf.tempfolder);
+            File.WriteAllText(sf.tempfolder, "тестовый текст");
         }
     }
 }
---
+++ Orbit_Launcher_3.0/StringField.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orbit_Launcher_3._0
+{
+    class StringField
+    {
+        public static string mainfolder = Environment.GetFolderPath(Environment.SpecialFolder.Programs) + "\\" + "Orbit_Launcher";
+        public static string tempfolder = mainfolder + "\\" + "Temp"; 
+
+
+    }
+}
