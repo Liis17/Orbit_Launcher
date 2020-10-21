93915e2fcdeda9f222191af7dcb83dba6a0c6e56
Li_is <daskent96@gmail.com>
2020-10-22 02:57:24 +0300

    что то новое

--- OrbitLaunch/MainWindow.xaml.cs
+++ OrbitLaunch/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,48 +18,83 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 
 
+
 namespace OrbitLaunch
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
 
         DispatcherTimer timer;
+        int time;
         public MainWindow()
         {
             InitializeComponent();
+            StartupProgressBar.Maximum = 2;
+            StartupProgressBar.Value = 0;
             SetTimer();
         }
 
         public void SetTimer()
         {
             timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(5);
+            timer.Interval = TimeSpan.FromMilliseconds(199);
             timer.Tick += OnTimedEvent;
             timer.Start();
-
-
         }
 
         public void OnTimedEvent(object sender, EventArgs e)
         {
+            CheckNetwork();
+            timer.Stop();
+            
+        }
 
-            if (StartupProgressBar.Value <= 100)
+        void CheckNetwork()
+        {
+            StartupTextBlock.Text = "Проверка соединения";
+            try
             {
-                StartupProgressBar.Foreground = Brushes.Blue;
+                WebClient web = new WebClient();
+                var adress = "https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Overwatch_cover_art.jpg/220px-Overwatch_cover_art.jpg";
+                var filename = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\overwatch.jpg";
+                web.DownloadFile(adress, filename);
+                StartupProgressBar.Value++;
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте соединение и посторите попытку","Отсутствует соединение");
+                Application.Current.Shutdown();
             }
 
-            if (StartupProgressBar.Value == 100)
+
+            CheckFile();
+        }
+
+        void CheckFile()
+        {
+            StartupTextBlock.Text = "Проверка файлов";
+
+
+            
+
+
+            StartupProgressBar.Value++;
+            LoadMainProgramm();
+        }
+
+        void LoadMainProgramm()
+        {
+            if (StartupProgressBar.Value == StartupProgressBar.Maximum)
             {
-                StartupProgressBar.Foreground = Brushes.Green;
                 timer.Stop();
+                StartupProgressBar.Foreground = Brushes.Green;
                 WindowLauncher win = new WindowLauncher();
                 win.Show();
                 StartWindow.Close();
             }
-            StartupProgressBar.Value += 1;
         }
     }
 }
